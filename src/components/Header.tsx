22368a606a26a65aafe033ea5e2f0673db604980
lidelong <lidelong0201@foxmail.com>
2024-01-12 00:36:46 +0800

    feat:更新菜单文案

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -94,7 +94,7 @@ function MobileNavigation(
   return (
     <Popover {...props}>
       <Popover.Button className="group flex items-center rounded-full bg-white/90 px-4 py-2 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10 dark:hover:ring-white/20">
-        Menu
+        菜单
         <ChevronDownIcon className="ml-3 h-auto w-2 stroke-zinc-500 group-hover:stroke-zinc-700 dark:group-hover:stroke-zinc-400" />
       </Popover.Button>
       <Transition.Root>
@@ -127,7 +127,7 @@ function MobileNavigation(
                 <CloseIcon className="h-6 w-6 text-zinc-500 dark:text-zinc-400" />
               </Popover.Button>
               <h2 className="text-sm font-medium text-zinc-600 dark:text-zinc-400">
-                Navigation
+                导航
               </h2>
             </div>
             <nav className="mt-6">
